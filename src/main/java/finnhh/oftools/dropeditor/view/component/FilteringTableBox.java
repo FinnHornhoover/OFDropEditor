335503d90c612d0ff5b416eaf280bdc7af52c479
FinnHornhoover <finnhornhoover@gmail.com>
2025-11-26 12:33:12 +0300

    make ReferenceListBox objects searchable

--- src/main/java/finnhh/oftools/dropeditor/view/component/FilteringTableBox.java
+++ src/main/java/finnhh/oftools/dropeditor/view/component/FilteringTableBox.java
@@ -1,9 +1,11 @@
 package finnhh.oftools.dropeditor.view.component;
 
+import javafx.animation.PauseTransition;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 import java.util.List;
 import java.util.Locale;
@@ -12,6 +14,7 @@ import java.util.function.Supplier;
 public class FilteringTableBox<T> extends VBox {
     private final TableView<T> tableView;
     private final TextField textField;
+    private final PauseTransition pause;
 
     public FilteringTableBox(final Supplier<List<T>> dataGetter) {
         tableView = new TableView<>();
@@ -19,13 +22,21 @@ public class FilteringTableBox<T> extends VBox {
         textField = new TextField();
         textField.getStyleClass().add("add-graphic-text-field");
         textField.setPromptText("Search by name or by e.g. level=8");
+
+        // Only update after the user stops typing (debounce with PauseTransition)
+        pause = new PauseTransition(Duration.millis(350));
+
         textField.textProperty().addListener((o, oldVal, newVal) -> {
-            tableView.getItems().clear();
-            tableView.getItems().addAll(dataGetter.get()
-                    .stream()
-                    .filter(t -> t.toString().toLowerCase(Locale.ENGLISH)
-                            .contains(newVal.toLowerCase(Locale.ENGLISH).trim()))
-                    .toList());
+            pause.stop();
+            pause.setOnFinished(e -> {
+                tableView.getItems().clear();
+                tableView.getItems().addAll(dataGetter.get()
+                        .stream()
+                        .filter(t -> t.toString().toLowerCase(Locale.ENGLISH)
+                                .contains(newVal.toLowerCase(Locale.ENGLISH).trim()))
+                        .toList());
+            });
+            pause.playFromStart();
         });
 
         setSpacing(8);
--- src/main/java/finnhh/oftools/dropeditor/view/component/ReferenceListBox.java
+++ src/main/java/finnhh/oftools/dropeditor/view/component/ReferenceListBox.java
@@ -1,7 +1,15 @@
 package finnhh.oftools.dropeditor.view.component;
 
 import finnhh.oftools.dropeditor.MainController;
+import finnhh.oftools.dropeditor.model.EventType;
+import finnhh.oftools.dropeditor.model.ItemType;
+import finnhh.oftools.dropeditor.model.data.CodeItem;
+import finnhh.oftools.dropeditor.model.data.Crate;
 import finnhh.oftools.dropeditor.model.data.Data;
+import finnhh.oftools.dropeditor.model.data.Event;
+import finnhh.oftools.dropeditor.model.data.ItemReference;
+import finnhh.oftools.dropeditor.model.data.Mob;
+import finnhh.oftools.dropeditor.model.data.Racing;
 import finnhh.oftools.dropeditor.view.util.NoSelectionModel;
 import javafx.beans.property.*;
 import javafx.collections.FXCollections;
@@ -10,15 +18,19 @@ import javafx.geometry.Orientation;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableCell;
+import javafx.util.Pair;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ReferenceListBox extends ListView<ReferenceTrailBox> {
     private final ListProperty<ReferenceTrailBox> referenceTrails;
     private final ObjectProperty<Data> originData;
+    private final StringProperty referenceString;
 
     private final MainController controller;
     private final double imageWidth;
@@ -26,11 +38,14 @@ public class ReferenceListBox extends ListView<ReferenceTrailBox> {
     public ReferenceListBox(double imageWidth, MainController controller, Data data) {
         referenceTrails = new SimpleListProperty<>(FXCollections.observableArrayList());
         originData = new SimpleObjectProperty<>(data);
+        referenceString = new SimpleStringProperty("");
 
         this.controller = controller;
         this.imageWidth = imageWidth;
 
         recurseTrails(data);
+        // we can exclude reference leaf data if it's not useful down the line
+        constructReferenceString(true);
 
         setOrientation(Orientation.HORIZONTAL);
         setEditable(false);
@@ -83,6 +98,75 @@ public class ReferenceListBox extends ListView<ReferenceTrailBox> {
         referenceTrails.addAll(trailList);
     }
 
+    private List<String> getDataRecordString(Data data) {
+        var mobTypeInfoMap = controller.getStaticDataStore().getMobTypeInfoMap();
+        var itemInfoMap = controller.getStaticDataStore().getItemInfoMap();
+        var epInstanceMap = controller.getStaticDataStore().getEPInstanceMap();
+        List<String> infoStrings = new ArrayList<>();
+
+        if (data instanceof CodeItem ci) {
+            // we only need to reference the actual code
+            infoStrings.add(ci.getCode());
+
+        } else if (data instanceof Crate c) {
+            // might need to reference the actual crate level etc.
+            var key = new Pair<>(c.getCrateID(), ItemType.CRATE.getTypeID());
+
+            Optional.ofNullable(itemInfoMap.get(key))
+                    .map(Object::toString)
+                    .ifPresent(infoStrings::add);
+
+        } else if (data instanceof Event e) {
+            // event name is enough but we have the type
+            EventType eventType = EventType.forType(e.getEventID());
+
+            if (eventType != EventType.NO_EVENT) {
+                infoStrings.add(eventType.toString());
+            }
+
+        } else if (data instanceof ItemReference ir) {
+            // we definitely need the item details
+            var key = new Pair<>(ir.getItemID(), ir.getType());
+
+            Optional.ofNullable(itemInfoMap.get(key))
+                    .map(Object::toString)
+                    .ifPresent(infoStrings::add);
+
+        } else if (data instanceof Mob m) {
+            // mob name and level is nice to be able to search
+            Optional.ofNullable(mobTypeInfoMap.get(m.getMobID()))
+                    .map(Object::toString)
+                    .ifPresent(infoStrings::add);
+
+        } else if (data instanceof Racing r) {
+            // searching by instance name is useful for racing edits
+            Optional.ofNullable(epInstanceMap.get(r.getEPID()))
+                    .map(Object::toString)
+                    .ifPresent(infoStrings::add);
+
+        } else {
+            // fallback, at least let the user search the ID
+            infoStrings.add(data.toString());
+        }
+
+        return infoStrings;
+    }
+
+    private void constructReferenceString(boolean includeReferenceLeafs) {
+        if (includeReferenceLeafs) {
+            referenceString.set(
+                Stream.concat(
+                    getDataRecordString(getOriginData()).stream(),
+                    getReferenceTrails().stream().flatMap(rt -> getDataRecordString(rt.getLastData()).stream()))
+                .collect(Collectors.joining(", "))
+            );
+        } else {
+            referenceString.set(
+                getDataRecordString(getOriginData()).stream().collect(Collectors.joining(", "))
+            );
+        }
+    }
+
     public Data getOriginData() {
         return originData.get();
     }
@@ -101,5 +185,10 @@ public class ReferenceListBox extends ListView<ReferenceTrailBox> {
 
     public double getImageWidth() {
         return imageWidth;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + ": " + referenceString.get();
     }
 }
